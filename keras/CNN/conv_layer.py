c6e9931084e32d17a02162186b0a351b509a5459
zzzzl13 <Q384726652@163.com>
2022-02-07 19:40:55 +0800

    卷积神经网络小结

--- keras/CNN/conv_layer.py
+++ keras/CNN/conv_layer.py
@@ -14,3 +14,18 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(filter = 2, kernel_size = 3, strides = 1, padding = 'same', activation = 'relu')
     # 配合其他内容
 ])
+
+
+'''
+在卷积神经网络中  通常还会有以下元素伴随者卷积层
+1. 批标准化（BN)
+对数据进行按batch进行批标准化处理，是数据回归到0为均值 1为标准差的分布，使得数据通过激活函数后更能明显体现数据微小变化带来的差异
+提升区别能力（如sigmoid激活函数 0附近的数据更贴近线性变化）
+2. 激活函数（Activation）
+3. 池化（pooling）
+用于减少特征数据量，降低数据冗余，常分为最大值池化和均值池化，分别作用为提取图片纹理和保留背景特征
+4. 舍弃（dropout）
+神经网络训练时，为缓解过拟合，可以使用按比例舍弃部分神经元，等再使用神经网络时再恢复
+
+卷积->特征提取器（CBAPD)
+'''
